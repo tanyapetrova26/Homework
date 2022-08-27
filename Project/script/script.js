26dc2b206f0a18ce008e0afb31fcfe18c061f7df
tanyapetrova26 <108935676+tanyapetrova26@users.noreply.github.com>
2022-08-27 16:32:38 +0300

    Add files via upload

---
+++ Project/script/script.js
@@ -0,0 +1,16 @@
+$('.company-items > div') .on('click', function(){
+   $(this).children('.company_icon').toggleClass('company-rotate_icon');
+   $(this).children('.company_subtitle').toggleClass('change-color');
+   $(this).parents('.company-items').toggleClass('change-color');
+   $(this).next('.company-text').slideToggle(300);
+
+});
+
+
+$('.Features-items > div') .on('click', function(){
+   $(this).children('.Features_icon').toggleClass('Features-rotate_icon');
+   $(this).children('.Features-items__title').toggleClass('change-color__Features');
+   $(this).parents('.Features-items').toggleClass('change-color__hr');
+   $(this).next('.Features-items__text').slideToggle(300);
+   
+})
