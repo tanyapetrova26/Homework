eda05e96c486a9496456566ec5045c7d89f24a0a
tanyapetrova26 <108935676+tanyapetrova26@users.noreply.github.com>
2022-09-12 14:12:07 +0300

    Add files via upload

---
+++ project be arc/script/upbutton.js
@@ -0,0 +1,12 @@
+$(window).on('scroll', function(){
+    if($(this).scrollTop()>500){
+        $('#up').fadeIn()
+    }
+    else{
+        $('#up').fadeOut()
+    }
+})
+
+$('#up').on('click', function(){
+    $('html, body').animate({scrollTop: 0}, 500)
+})
